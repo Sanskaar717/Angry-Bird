208847cd087e89b0f60cc6ebd4c251f6e45cd3dd
Sanskaar717 <73640122+Sanskaar717@users.noreply.github.com>
2020-11-22 09:52:55 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,35 +3,18 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 
 var myEngine, myWorld;
-var ground,box2,box3;
+var ground,box,box2;
 
 function setup(){
-  createCanvas(600,600);
+  createCanvas(1200,500);
 
   myEngine = Engine.create(); //myEngine.World
   myWorld = myEngine.world;
 
-  var op = {
-    isStatic : true
-  }
+  ground = new Ground(width/2,490,width,20);
 
-  var rest = {
-    restitution : 2
-  }
-
-  var rest1 = {
-    restitution : 1
-  }
-
-  ground = Bodies.rectangle(300,590,600,10,op);
-  World.add(myWorld,ground);
-
-  box2 = Bodies.rectangle(300,100,50,50,rest);
-  World.add(myWorld,box2);
-
-  box3 = Bodies.rectangle(125,25,50,50,op);
-  World.add(myWorld,box3);
-
+  box = new Box(width/2,height/2,50,50);
+  box2 = new Box(620,100,50,100);
 }
 
 function draw(){
@@ -40,9 +23,7 @@ function draw(){
 
   Engine.update(myEngine);
 
-  rectMode(CENTER);
-  rect(ground.position.x,ground.position.y,600,10);
-  rect(box2.position.x,box2.position.y,50,50);
-  rect(box3.position.x,box3.position.y,50,50);
-
+  ground.display();
+  box.display();
+  box2.display();
 }
