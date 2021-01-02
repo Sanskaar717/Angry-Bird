42b068926e8e183cd08c7096ac6841bacef39825
Sanskaar717 <73640122+Sanskaar717@users.noreply.github.com>
2021-01-02 09:59:15 +0530

    Add files via upload

---
+++ Base.js
@@ -0,0 +1,30 @@
+class Base{
+
+    constructor(x,y,width,height,angle){
+        
+   var rest1 = {
+     restitution : 0.5,
+     friction : 1,
+     density : 1
+   }
+ 
+   this.body = Bodies.rectangle(x,y,width,height,rest1);
+   this.width = width;
+   this.height = height;
+   this.image = loadImage("sprites/base.png");
+   World.add(myWorld,this.body);
+    }
+ 
+    display(){
+        var pos = this.body.position;
+        var angle = this.body.angle;  
+        push();
+        translate(pos.x,pos.y);
+        rotate(angle);
+        fill("blue");
+        imageMode(CENTER);
+        image(this.image,0,0,this.width,this.height);
+        pop();
+    }
+ 
+ }
