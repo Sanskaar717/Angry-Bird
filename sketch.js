078af001d209c5da1ff5048c8278a011c8b24785
Sanskaar717 <73640122+Sanskaar717@users.noreply.github.com>
2021-01-16 10:58:40 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,48 +1,102 @@
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint; 
 
 var myEngine, myWorld;
-var ground,box2,box3;
+var ground,box,box2,pig,pig2;
+var bird,log,log2,log3,log4;
+var box3, box4,box5;
+var back,bg,score = 0;
+
+function preload(){
+  getTime();
+}
 
 function setup(){
-  createCanvas(600,600);
+  createCanvas(1200,600);
 
   myEngine = Engine.create(); //myEngine.World
   myWorld = myEngine.world;
 
-  var op = {
-    isStatic : true
-  }
-
-  var rest = {
-    restitution : 2
-  }
-
-  var rest1 = {
-    restitution : 1
-  }
-
-  ground = Bodies.rectangle(300,590,600,10,op);
-  World.add(myWorld,ground);
-
-  box2 = Bodies.rectangle(300,100,50,50,rest);
-  World.add(myWorld,box2);
-
-  box3 = Bodies.rectangle(125,25,50,50,op);
-  World.add(myWorld,box3);
-
+  ground = new Ground(width/2,590,width,20);
+  platform = new Ground(170,450,350,300);
+  box = new Box(800,550);
+  box2 = new Box(1000,550);
+  box3 = new Box(800,450);
+  box4 = new Box(1000,450);
+  box5 = new Box(900,350);
+  pig = new Pig(900,550);
+  pig2 = new Pig(900,450);
+  bird = new Bird(200,225);
+  log = new Log(900,500,300,PI/2);
+  log2 = new Log(900,400,300,PI/2);
+  log3 = new Log(800,350,150,PI/7);
+  log4 = new Log(1000,350,150,-PI/7);
+  sling = new Slingshot(bird.body,{x:250,y:140});
+  
 }
 
 function draw(){
 
-  background(0);
+  if(back){ 
+  background(back);
+  push();
+  textSize(25);
+  fill(0);
+  stroke(255);
+  strokeWeight(2);
+  text("Score:"+score,1000,50);
+  pop();
+  }
 
   Engine.update(myEngine);
 
-  rectMode(CENTER);
-  rect(ground.position.x,ground.position.y,600,10);
-  rect(box2.position.x,box2.position.y,50,50);
-  rect(box3.position.x,box3.position.y,50,50);
+  ground.display();
+  box.display();
+  box2.display();
+  box3.display();
+  box4.display();
+  box5.display();
+  pig.display();
+  pig2.display();
+  bird.display();
+  log.display();
+  log2.display();
+  log3.display();
+  log4.display();
+  platform.display();
+  sling.display();
+  
+  pig.scoreInc();
+  pig2.scoreInc();
+}
 
+function mouseDragged(){
+  Matter.Body.setPosition(bird.body,{x:mouseX,y:mouseY});
+}
+
+function mouseReleased(){
+  sling.fly();
+}
+
+function keyPressed(){
+  if(keyCode === 32){
+    sling.attach(bird.body);
+  }
+}
+
+async function getTime(){
+  var res = await fetch("http://worldclockapi.com/api/json/est/now");
+  var resp = await res.json();
+  var dt = resp.currentDateTime;
+  var hr = dt.slice(11,13);
+  console.log(hr);
+  if(hr>=06 && hr<=18){
+    bg = "sprites/bg2.jpg";
+  }
+  else{
+    bg = "sprites/bg.png";
+  }
+  back = loadImage(bg);
 }
